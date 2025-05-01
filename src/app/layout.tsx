3deb0084ea4b6fd8fdf55ee18822993efaebb77e
Lucas Raza <Lmraza98@gmail.com>
2025-05-01 01:22:09 -0400

    added @vercel/analytics to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,20 @@
-import 'dotenv/config'
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { GoogleAnalytics } from '@next/third-parties/google'
-import "./globals.css";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "HIPAA-Compliant App Development | Fast, Secure, Certified",
-  description: "Build your HIPAA-compliant healthcare app with specialists who understand HIPAA, PHI, and secure architecture - launch with confidence.",
-};
+import { Analytics } from '@vercel/analytics/next';
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
         {children}
+        <Analytics />
       </body>
-      <GoogleAnalytics  gaId="G-XYZ" />
     </html>
   );
 }
+
